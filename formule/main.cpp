7872fae717ec5aad1f91f4b21443fa9e838f6829
Snejochekk <Snejochekk@mail.ru>
2022-08-31 21:11:21 +0300

    попытка

---
+++ formule/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+
+{
+    const double a=cos(2);
+    double x;
+    const double c=M_PI/6;
+    const double d=sqrt(2);
+    const double f=1/log2(x);
+    const double g=pow(sin(pow(x,2)),2);
+    const double q=exp(3);
+    double y;
+
+    printf("input x=");
+    y=((2*(cos(x-(M_PI/6))+sqrt(2)))/((1/(2*log(x)))+pow(sin(pow(x,2)),2)))*exp(3*x);
+    scanf("%lf",&x,a,c,d,f,g,q,y);
+
+    return 0;
+}
+
