1ca9d7bcf1ec0dbecf9dadf0e924fe98e3056a26
Snejochekk <Snejochekk@mail.ru>
2022-09-08 18:43:52 +0300

    Исправление

--- formule/main.cpp
+++ formule/main.cpp
@@ -1,22 +1,19 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+
+//Исправила
 
 int main()
-
 {
-    const double a=cos(2);
     double x;
-    const double c=M_PI/6;
-    const double d=sqrt(2);
-    const double f=1/log2(x);
-    const double g=pow(sin(pow(x,2)),2);
-    const double q=exp(3);
-    double y;
-
     printf("input x=");
-    y=((2*(cos(x-(M_PI/6))+sqrt(2)))/((1/(2*log(x)))+pow(sin(pow(x,2)),2)))*exp(3*x);
-    scanf("%lf",&x,a,c,d,f,g,q,y);
+    scanf("%lf",&x);
+
+    const double a=(cos(2)*(x-(M_PI/6))+sqrt(2));
+    const double c=1/log2(x)+pow(sin(pow(x,2)),2);
+    const double d=exp(3);
+    const double y=(a/c)*d;
+    printf("Result %f",y);
 
     return 0;
 }
-
